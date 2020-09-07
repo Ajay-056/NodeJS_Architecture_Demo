1dadeb7b9c5685bea35aef2ea0f7c9b54175444e
AK <balaajay19@gmail.com>
2020-09-07 21:09:02 +0530

    Used shorthand for module export

--- modules.js
+++ modules.js
@@ -7,5 +7,6 @@ const calc1 = new calc();
 
 console.log(calc1.add(5, 6));
 
-const calc2 = require("./test-module-2");
-console.log(calc2.add(5, 6));
+const { add, subtract, multiply, divide } = require("./test-module-2");
+console.log(add(5, 6));
+console.log(multiply(5, 6));
