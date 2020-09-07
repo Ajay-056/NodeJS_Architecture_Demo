211dbbfeda05fcde5cc08fec54a565a6bf39e2ea
AK <balaajay19@gmail.com>
2020-09-07 14:35:24 +0530

    First successfull run

---
+++ event-loop.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
+});
+
+console.log("Hello, from top-level code!");
