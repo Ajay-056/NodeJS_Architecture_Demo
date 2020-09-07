12978b814ed48acbd6e388c23d471a50e0b9c27e
AK <balaajay19@gmail.com>
2020-09-07 14:42:09 +0530

    Added some code to event loop in a seperated way

--- event-loop.js
+++ event-loop.js
@@ -5,6 +5,15 @@ setImmediate(() => console.log("Immediate 1 finished"));
 
 fs.readFile("test-file.txt", () => {
   console.log("I/O finished");
+  console.log("---------------");
+  console.log("Inside event-loop");
+  console.log("---------------");
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
 });
 
+console.log("---------------");
+console.log("Outside event-loop");
+console.log("---------------");
 console.log("Hello, from top-level code!");
