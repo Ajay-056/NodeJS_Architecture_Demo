bb745c9a4fd505c39c1d2a34b5e24783b1653464
AK <balaajay19@gmail.com>
2020-09-07 18:47:06 +0530

    Used calc module in modules.js

--- modules.js
+++ modules.js
@@ -1,2 +1,8 @@
-console.log(arguments);
-console.log(require("module").wrapper);
+// console.log(arguments);
+// console.log(require("module").wrapper);
+
+const calc = require("./test-module-1");
+
+const calc1 = new calc();
+
+console.log(calc1.add(5, 6));
