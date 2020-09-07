8f9599e1202e62ac7470c0257ecc584511e3ce43
AK <balaajay19@gmail.com>
2020-09-07 18:03:06 +0530

    Used pipe() with stream

--- streams.js
+++ streams.js
@@ -2,19 +2,9 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  //Solution 2 : Using Streams
+  //Solution 3 : Using pipe() with stream to avoid backpressure
   const readable = fs.createReadStream("test-file.txt");
-  readable.on("data", (chunk) => {
-    res.write(chunk); //This is a write stream by default
-  });
-  readable.on("end", () => {
-    res.end();
-  });
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found!");
-  });
+  readable.pipe(res);
 });
 
 server.listen("8000", () => {
