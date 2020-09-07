c6451567d6c4f22e7f40feac938a8ab78d4a6029
AK <balaajay19@gmail.com>
2020-09-07 15:43:33 +0530

    Used the Synchronous way instead of Asynchronous

--- event-loop.js
+++ event-loop.js
@@ -18,18 +18,17 @@ fs.readFile("test-file.txt", () => {
 
   process.nextTick(() => console.log("Process.nextTick"));
 
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted");
-  });
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted");
-  });
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted");
-  });
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted");
-  });
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
 });
 
 console.log("---------------");
