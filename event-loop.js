e6f3d6b87f1e648e5bce83b233f63c3fc280eb4c
AK <balaajay19@gmail.com>
2020-09-07 15:02:09 +0530

    Added nextTick to event loop

--- event-loop.js
+++ event-loop.js
@@ -11,6 +11,8 @@ fs.readFile("test-file.txt", () => {
   setTimeout(() => console.log("Timer 2 finished"), 0);
   setTimeout(() => console.log("Timer 3 finished"), 3000);
   setImmediate(() => console.log("Immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process.nextTick"));
 });
 
 console.log("---------------");
