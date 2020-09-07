068826fb63de4a74c40684f065fce93fb14648dd
AK <balaajay19@gmail.com>
2020-09-07 15:18:06 +0530

    Implmented thread pool for 4 threads at same time

--- event-loop.js
+++ event-loop.js
@@ -20,6 +20,15 @@ fs.readFile("test-file.txt", () => {
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "Password encrypted");
   });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 });
 
 console.log("---------------");
