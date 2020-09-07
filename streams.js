f3640751cffbe554a614c4b2b1baae7943c61c79
AK <balaajay19@gmail.com>
2020-09-07 17:34:14 +0530

    Used Stream to read text file

--- streams.js
+++ streams.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  //Solution 1 : Without using streams
-  fs.readFile("test-file.txt", "utf-8", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  //Solution 2 : Using Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk); //This is a write stream by default
+  });
+  readable.on("end", () => {
+    res.end();
   });
 });
 
